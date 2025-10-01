71aec73f8558313698a2a3e34de28c57756bf7c9
shreyasmore52 <shreyasmore52@gmail.com>
2025-10-01 13:04:01 +0530

    add Stack Navigator on file

--- Day3Assignment/frontend/App.js
+++ Day3Assignment/frontend/App.js
@@ -1,8 +1,20 @@
-
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/loginscreen';
+import { StackScreen } from 'react-native-screens';
+import RegisterScreen from './screens/RegisterScreen';
+const Stack  = createNativeStackNavigator();
 export default function App(){
 
     return <>
-   
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName='Register'>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <StackScreen name="Login" component={LoginScreen} />
+                    <StackScreen name="Register"component={RegisterScreen} />
+                 </Stack.Navigator>
+        </NavigationContainer>
     </>
 }
 
