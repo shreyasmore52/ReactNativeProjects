c50254a9bca81cd5755aece0aa6049d389b8f2bf
shreyasmore52 <shreyasmore52@gmail.com>
2025-10-04 17:13:40 +0530

    create routes

---
+++ Day4Assignment/backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const envFile = require("dotenv");
+envFile.config();
+const { userRouter } = require("./routes/userRouter");
+const pool = require("./db");
+const app = express();
+// parse JSON
+app.use(express.json());
+
+// Routes
+app.use("/api/user", userRouter);
+
+app.get("/", function(req,res){
+    res.json({
+        msg: "hello from backend"
+    })
+})
+
+app.listen(3004);
