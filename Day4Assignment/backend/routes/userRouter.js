83a0b7664ead4d16d606913314806fcff95d0e05
shreyasmore52 <shreyasmore52@gmail.com>
2025-10-04 17:15:21 +0530

    add routes for user

---
+++ Day4Assignment/backend/routes/userRouter.js
@@ -0,0 +1,88 @@
+const { Router } = require("express");
+const userRouter = Router();
+const { userAuthMiddleware, JWT_SECRET } = require("../Middleware/userAuthMiddleware"); 
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const pool = require("../db");
+
+userRouter.post("/verify", userAuthMiddleware, (req, res) => {
+  res.json({
+    success: true,
+    message: "Token is valid",
+    user: req.user, // comes from middleware
+  });
+});
+
+userRouter.post("/signUp", async function(req,res){
+    const {email, password ,firstname, lastname} = req.body;
+     
+    try{
+         const checkUser = await pool.query("SELECT * FROM users WHERE email=$1", [email]);
+         if(checkUser.rows.length > 0){
+            return res.json({
+                meg:"User Exists"
+            });
+         }
+             const hashPass = await bcrypt.hash(password, 5);
+             const result = await pool.query(
+                "INSERT INTO users (email, password, firstname, lastname) VALUES ($1,$2,$3,$4) RETURNING *",
+                [email, hashPass, firstname, lastname]);
+
+             res.json({
+                msg: "user creted",
+                user: result.rows[0]
+             });
+            
+    }catch(e){
+            console.error("Signup error:", e);
+            res.status(500).json({ error: "Server error" });       
+    }
+});
+
+userRouter.post("/logIn", async function(req,res){
+    const { email, password } = req.body;
+
+    const user = await pool.query("SELECT * FROM users WHERE email=$1", [email]);
+    const foundUser = user.rows[0];
+    try{
+        if(!foundUser){
+            return res.json({
+                msg: "User not exits"
+            });
+        }
+        const checkPass = await bcrypt.compare(password, foundUser.password);
+        if(checkPass){
+            const token = jwt.sign({
+            userId : foundUser.id
+            },JWT_SECRET);
+
+            return res.json({
+            msg: "Logged in", 
+            token: token
+            });
+        }
+     
+    } catch(e){
+         console.error("Login error:", e);
+        res.status(500).json({ error: "Server error" });
+    }
+
+});
+
+userRouter.get("/home", userAuthMiddleware ,async function(req,res){
+    try{
+        const data = await pool.query(
+            "SELECT firstname FROM users WHERE id=$1", [req.userId]);
+        res.json({
+           msg: "Welcome home!", 
+           user: data.rows[0] 
+        });
+
+    }catch(e){
+        console.error("Error on ->", e);   
+        res.status(500).json({ error: e.message });
+    }
+});
+
+
+module.exports = {userRouter};
