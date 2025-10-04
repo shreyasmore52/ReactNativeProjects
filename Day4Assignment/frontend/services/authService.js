d53f0f5964dbeb8d9ab4feed98fd67deeab7f576
shreyasmore52 <shreyasmore52@gmail.com>
2025-10-04 16:27:18 +0530

    add authservice

--- Day4Assignment/frontend/services/authService.js
+++ Day4Assignment/frontend/services/authService.js
@@ -1,3 +1,29 @@
-export function hi (){
+import axiosInstance  from "./axiosInstance";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+export default async function loginUserCheck(email, password){
+    try{
+        const response = await axiosInstance.post("/logIn", {
+            email,
+            password
+        });
+
+        const token = response.data.token;
+
+        if (!token) {
+          console.log("No token received from server:", response.data);
+          return { success: false, message: response.data?.msg || "Login failed" };
+        }
+
+        await AsyncStorage.setItem("userToken", token);
+        
+        return {
+            success: true,
+            token: token
+        }
+    }catch(e){
+         return { 
+            success: false,
+            message: e.message || "Something went wrong" };
+    }
 }
