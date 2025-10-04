1328f219f28aac588c97768ed5cbf16fee3cdb34
shreyasmore52 <shreyasmore52@gmail.com>
2025-10-04 15:46:34 +0530

    add stack router for navigation

--- Day4Assignment/frontend/App.js
+++ Day4Assignment/frontend/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import  RegisterScreen from './screen/signup'
+import LogInScreen from './screen/login'
+import HomeScreen from './screen/home'
+import ProfileScreen from './screen/profile'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+
+  return <>
+          <NavigationContainer>
+              <Stack.Navigator>
+                <Stack.Screen name="SignUp" 
+                component={RegisterScreen}
+                options={{
+                  headerShown: false
+                }}
+                />
+                <Stack.Screen name="LogIn" 
+                  component={LogInScreen}
+                   options={{
+                  headerShown: false
+                }}
+                />
+                <Stack.Screen name="Home"
+                component={HomeScreen}
+                   options={{
+                  headerShown: false
+                }}
+                />
+                <Stack.Screen name="Profile"
+                component={ProfileScreen}
+                   options={{
+                  headerShown: false
+                }}
+                />
+              </Stack.Navigator>
+          </NavigationContainer>
+  </>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
